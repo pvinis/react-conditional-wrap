1d59c96c98300789cc3828778d1e11123875a62e
Pavlos Vinieratos <pvinis@gmail.com>
2024-06-29 03:16:23 +0300

    test

---
+++ example/README.md
@@ -0,0 +1,3 @@
+# Example
+
+Look at the example in [`App.tsx`](./src/App.tsx) to see a usage of `Wrap`.
---
+++ src/Wrap.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren } from "react";
+import { getChildrenByTypeDeep } from "react-nanny";
+
+interface WrapProps {
+  if: boolean;
+}
+
+export function Wrap({
+  if: condition,
+  children,
+}: PropsWithChildren<WrapProps>): JSX.Element {
+  if (condition) {
+    return <>{children}</>;
+  }
+
+  const wrapContentChildren = getChildrenByTypeDeep(children, Wrap.Content);
+  if (wrapContentChildren.length === 0) {
+    throw new Error("Wrap.Content is required");
+  }
+  if (wrapContentChildren.length > 1) {
+    throw new Error("You can't have more than one Wrap.Content");
+  }
+
+  const actualWrapContent = wrapContentChildren[0];
+  return <>{actualWrapContent}</>;
+}
+
+Wrap.Content = WrapContent;
+function WrapContent({ children }: PropsWithChildren): JSX.Element {
+  return <>{children}</>;
+}
+
